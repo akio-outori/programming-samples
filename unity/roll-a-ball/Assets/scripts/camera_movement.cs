e9f2aba41869841de440ee1bb3b7d064cd6bbac4
Jeff Hallyburton <jeff.hallyburton@thomsonreuters.com>
2018-01-28 15:05:14 -0500

    add initial unity project.

---
+++ unity/roll-a-ball/Assets/scripts/camera_movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_movement : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	// LateUpdate is called once all items in Update have been processed
+	// This means that we know for sure that the player has moved for that frame before we try to re-position the camera.
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
